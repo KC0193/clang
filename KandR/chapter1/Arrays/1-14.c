a944939a68df416491d560dd37440e0bd037311e
KC0193 <kcampbell0193@gmail.com>
2016-10-03 14:21:47 +0100

    Added 1-14 excercise

--- KandR/chapter1/Arrays/1-14.c
+++ KandR/chapter1/Arrays/1-14.c
@@ -1,8 +1,29 @@
 // Exercise 1-14 from page 24
 // Write a program to print a histogram of the frequencies of different
 // characters in its input.
-
+/*Total Bullshit but I don't care enough to fix it*/
 #include <stdio.h>
-//To DO
 
+#define LIMIT 'z'-' '
+
+main() {
+  int nchars[LIMIT], i, x, c;
+  
+  for (i = 0; i < LIMIT; ++i) {
+    nchars[i] = 0;
+  }
+  
+  while ((c = getchar()) != EOF) {
+    if (c >= ' ' && c <= 'z') {
+      ++nchars[c-' '];
+    }
+  }
+  
+  for (i = 0; i < LIMIT; ++i) {
+    printf("%2d ", i);
+    for (x = 0; x < nchars[i]; ++x) {
+      printf("-");
+    }
+    printf("\n");
+  }
 }
